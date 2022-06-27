ed9e79c5daebc984a12706fab44eeb33ab09c466
Jacob Klein <jacob23791@gmail.com>
2022-06-27 21:31:48 +1000

    Production Build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,22 @@
 source "common.sh"
 set -e
 
-cd ${BASE_DIR}
-npm run build
-cp ${BASE_DIR}/404.html ${BASE_DIR}/docs/404.html
+#cd ${BASE_DIR}
+#npm run build
+#cp ${BASE_DIR}/404.html ${BASE_DIR}/docs/404.html
 
-#cd "${BASE_DIR}" || exit 1
-#echoCyan "${BASE_DIR}"
-#echoCyan "Committing code"
-#git add -A
-#git commit -m "Production Build"
-#git push
+#===================== Legacy =======================#
+cd ${BASE_DIR}
+rm -rf docs
+mkdir docs
+echo "${BASE_DIR}/legacy"
+cp -r ${BASE_DIR}/legacy/. ${BASE_DIR}/docs/
+
+#=============== Commit ====================#
+
+cd "${BASE_DIR}" || exit 1
+echoCyan "${BASE_DIR}"
+echoCyan "Committing code"
+git add -A
+git commit -m "Production Build"
+git push
