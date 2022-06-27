36f13d989cf3b7c413e9c4413d2cc9e3ea3699f5
Jacob Klein <jacob23791@gmail.com>
2022-06-27 21:14:10 +1000

    WIP

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,9 +6,9 @@ cd ${BASE_DIR}
 npm run build
 cp ${BASE_DIR}/404.html ${BASE_DIR}/docs/404.html
 
-cd "${BASE_DIR}" || exit 1
-echoCyan "${BASE_DIR}"
-echoCyan "Committing code"
-git add -A
-git commit -m "Production Build"
-git push
+#cd "${BASE_DIR}" || exit 1
+#echoCyan "${BASE_DIR}"
+#echoCyan "Committing code"
+#git add -A
+#git commit -m "Production Build"
+#git push
